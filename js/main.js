8e46ac67c8175cf1d079328bfcc35aeee1f19f41
Loquen Jones <lrjones416@gmail.com>
2019-07-26 10:29:38 -0700

    update styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 /*----- constants -----*/ 
+const P1 = 'X';
+const P2 = 'O';
+let TURN = 1;
+
 /*----- app's state (variables) -----*/ 
+
 /*----- cached element references -----*/ 
+let board = document.getElementById('game-board');
+let reset = document.getElementById('reset');
+
 /*----- event listeners -----*/ 
+board.addEventListener('click', function(evt){
+    console.log(evt.target);
+    if(TURN === 1){
+        evt.target.textContent = P1;
+        TURN = -1;
+    } else {
+        evt.target.textContent = P2;
+        TURN = 1;
+    }
+});
+
+reset.addEventListener('click', function(evt){
+    console.log('reset');
+});
+
 /*----- functions -----*/
